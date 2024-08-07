14470102632f010bae85e455973056da46168cd8
fmatovelle <167099575+fmatovelle@users.noreply.github.com>
2024-08-07 22:18:52 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function generateDomains(pronoun, adj, noun) {
+    let domains = [];
+    for (let p of pronoun) {
+      for (let a of adj) {
+        for (let n of noun) {
+          domains.push(`${p}${a}${n}.com`);
+        }
+      }
+    }
+    return domains;
+  }
+
+  let domainNames = generateDomains(pronoun, adj, noun);
+  for (let domain of domainNames) {
+    console.log(domain);
+  }
 };
